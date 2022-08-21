db12e103dffec7c1f1870759e2af052505ecaa37
milywendland <milynicwendland@gmail.com>
2022-08-21 16:10:26 -0600

    fixed seed typo

--- seed/notes.js
+++ seed/notes.js
@@ -23,7 +23,7 @@ const main = async () => {
         'https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
     }
   ]
-  await Note.instertMany(notes)
+  await Note.insertMany(notes)
 
   console.log('Created Notes!')
 }
